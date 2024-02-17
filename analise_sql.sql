20a4ab5162adc9e1092bf9a8622a9396cefce1e4
marcelo-drummond-fonseca <marcelo.diedara@gmail.com>
2024-02-17 05:27:50 -0300

    Consertando erros e melhorando as respostas

--- analise_sql.sql
+++ analise_sql.sql
@@ -31,7 +31,7 @@ GROUP BY adm.id_bairro, bai.nome
 ORDER BY quantidade DESC
 LIMIT 3
 
---Resposta obtida: Engenho de Dentro, Campo Grande e Leblon
+--Resposta obtida: Engenho de Dentro (8 chamados), Campo Grande (6 chamados) e Leblon (6 chamados)
 
 
 
@@ -57,7 +57,8 @@ WHERE DATE(data_inicio) = '2023-04-01'
 AND adm.id_bairro IS NULL
 LIMIT 1
 
---Resposta obtida: Houve um caso, mas estou incerto da razão de ter ocorrído.
+--Resposta obtida: Houve um caso, de ID 18516246, com subtipo "Verificação de ar condicionado inoperante no ônibus" em tipo "Ônibus".
+--Não tenho certeza da razão de não ter um bairro especificado, é possível que esses dados não tenham sido inseridos ou informados junto ao chamado, ou que esse tipo de chamado não requer esses dados.
 
 
 
@@ -66,8 +67,7 @@ LIMIT 1
 SELECT adm.subtipo, COUNT(*) as quantidade
 FROM datario.administracao_servicos_publicos.chamado_1746 as adm
 WHERE adm.subtipo = 'Perturbação do sossego'
-AND DATE(adm.data_inicio) >= '2022-01-01'
-AND DATE(adm.data_inicio) <= '2023-12-31'
+AND DATE(adm.data_inicio) between '2022-01-01' and '2023-12-31'
 GROUP BY adm.subtipo
 
 --Resposta obtida: 42408 chamados com o subtipo Perturbação do Sossego no período.
@@ -78,11 +78,11 @@ GROUP BY adm.subtipo
 
 SELECT adm.*
 FROM datario.administracao_servicos_publicos.chamado_1746 as adm
-INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos as ev on adm.data_inicio between ev.data_inicial and ev.data_final
+INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos as ev on DATE(adm.data_inicio) between ev.data_inicial and ev.data_final
 WHERE adm.subtipo = 'Perturbação do sossego'
 AND ev.evento in ('Reveillon', 'Carnaval', 'Rock in Rio')
 
---Resposta obtida: Multiplos chamados durante os períodos.
+--Resposta obtida: Foi selecionada uma lista de 1212 resultados no total.
 
 
 
@@ -90,12 +90,12 @@ AND ev.evento in ('Reveillon', 'Carnaval', 'Rock in Rio')
 
 SELECT ev.evento, COUNT(*) as quantidade
 FROM datario.administracao_servicos_publicos.chamado_1746 as adm
-INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos as ev on adm.data_inicio between ev.data_inicial and ev.data_final
+INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos as ev on DATE(adm.data_inicio) between ev.data_inicial and ev.data_final
 WHERE adm.subtipo = 'Perturbação do sossego'
 AND ev.evento in ('Reveillon', 'Carnaval', 'Rock in Rio')
 GROUP BY ev.evento
 
---Resposta obtida: 79 pro Reveillon, 518 pro Rock in Rio, 197 pro Carnaval
+--Resposta obtida: 137 pro Reveillon, 834 pro Rock in Rio, 241 pro Carnaval
 
 
 
@@ -103,19 +103,21 @@ GROUP BY ev.evento
 
 SELECT ev.evento, COUNT(*) as quantidade, DATE_DIFF(ev.data_final, ev.data_inicial, DAY) + 1 as dias
 FROM datario.administracao_servicos_publicos.chamado_1746 as adm
-INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos as ev on adm.data_inicio between ev.data_inicial and ev.data_final
+INNER JOIN datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos as ev on DATE(adm.data_inicio) between ev.data_inicial and ev.data_final
 WHERE adm.subtipo = 'Perturbação do sossego'
 AND ev.evento in ('Reveillon', 'Carnaval', 'Rock in Rio')
 GROUP BY ev.evento, dias
 
---Resposta obtida: O Reveillon e Carnaval foram marcados como tendo 3 e 4 dias respectivamente, e tiveram 79 e 197 chamados respectivamente
---Assim, media diária de 26.3 pro Reveilllon e 49.25 pro Carnaval.
---O Rock in Rio foi separado em dois grupos, um com 3 dias e um com 4 dias, com 212 e 306 chamados respectivamente.
---Em ambos casos, a média é maior que 70 chamados por dia, então de qualquer forma, Rock in Rio é o que tem a maior quantidade de chamadas diárias, em média.
+--Resposta obtida: O Reveillon e Carnaval foram marcados como tendo 3 e 4 dias respectivamente, e tiveram 137 e 241 chamados respectivamente
+--Assim, media diária de ~46 pro Reveilllon e ~60 pro Carnaval.
+--O Rock in Rio foi separado em dois eventos, um com 3 dias e um com 4 dias, com 366 e 468 chamados respectivamente.
+--Isso totaliza 834 chamados em 7 dias, aproximadamente 119 chamados diários.
 
 
 
 -- Questão 10
 
---Resposta: O período de 2 anos tem média diária de chamados de aproximadamente 42408/730 ~= 58 chamadas diárias.
---Curiosamente, essa média é maior que a média de chamadas durante o Carnaval e Reveillon, mas menor que a do Rock in Rio
+--Resposta: Já possuimos os dados para responder essa questão. 
+--O período de 2 anos tem média diária de chamados de aproximadamente 42408/730 ~= 58 chamadas diárias.
+--Curiosamente, essa média é maior que a média de chamadas durante o Reveillon, levemente menor que a média de chamadas durante o carnaval, 
+--e significativamente menor que a do Rock in Rio
